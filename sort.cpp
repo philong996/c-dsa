1bce6c67acfe1c0116c584be9b7028e28bba4f1d
long.nguyenpp <long.nguyenpp@homecredit.vn>
2023-11-01 18:06:30 +0700

    add swap for bubble sort algorithm

--- sort.cpp
+++ sort.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
+#include <bits/stdc++.h> 
 
 using namespace std;
 
 
 /*
-Interchange sort
-*/
-void interchangeSort(int arr[], int n)
-{
-    int i = 0;
-
-    while ( i < n)
-    {
-        int j,i, temp;
-        for (int j = i + 1; j < n; j++)
-        {
-            if ( arr[j] < arr[i] )
-            {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        i++;
-    }
-}
-
-/*
 Bubble sort and apply adaptive
 Interchange and bubble sort are the same
 */
+void swap(int& a, int& b)
+{
+    int temp = b;
+    b = a;
+    a = temp;
+}
+
 void bubbleSort(int arr[], int n)
 {
     int flag;
@@ -40,9 +25,7 @@ void bubbleSort(int arr[], int n)
         {
             if (arr[j] > arr[j+1])
             {
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
+                swap(arr[j], arr[j+1]);
                 flag = 1;
             }
         }
@@ -64,14 +47,15 @@ void printArray(int arr[], int n)
 
 int main()
 {
-    int arr[] = {8, 7, 5 , 4, 3, 2, 1};
-    int n = 7;
+    int arr[] = {95, 45, 48, 98, 1, 485, 65, 478, 1, 2325};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    std::cout << "Original array: ";
+    cout << "Original array: ";
     printArray(arr, n);
+    
     bubbleSort(arr, n);
-
-    std::cout << "The sorted array: ";
+    
+    cout << "The sorted array: ";
     printArray(arr, n);
 
     return 0;

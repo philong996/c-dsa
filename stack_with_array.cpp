4c8b67aaa6350677496f057f578632914bf69e1a
philong996 <philong.996@gmail.com>
2023-11-27 23:19:14 +0700

    add some basic functionality of stack

--- stack_with_array.cpp
+++ stack_with_array.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct ArrayStack
 {
     int size;
-    int *stackData;
+    int *data;
     int top;
 };
 
@@ -20,29 +20,69 @@ isEmpty
 isFull
 */
 
-void initializeStack(ArrayStack* stack, int size)
+void initializeStack(ArrayStack *stack, int size)
 {
     stack->size = size;
-    stack->stackData = new int[size];
+    stack->data = new int[size];
     stack->top = -1;
+}
+
+int isFull(ArrayStack* stack)
+{
+    if (stack->top < stack->size - 1)
+        return 0;
+    else
+        return 1;
+}
+
+int isEmpty(ArrayStack* stack)
+{
+    if (stack->top == -1)
+        return 1;
+    else
+        return 0;
 }
 
 void push(ArrayStack* stack, int value)
 {
-    stack->stackData[stack->top++] = value;
+    if (isFull(stack) == 0)
+        stack->data[++stack->top] = value;
+    else
+        printf("Cannot append the value %d to the stack because it is full\n", value);
 }
+
+int pop(ArrayStack* stack)
+{
+    if (isEmpty(stack) == 1)
+    {
+        printf("The stack is empty");
+    }
+
+    return stack->data[stack->top--];
+}
+
+
 
 void printStack(ArrayStack &stack)
 {
-    printf("Stack size: %d", stack.size);
-    printf("Stack top: %d", stack.top);
+    printf("Stack size: %d \n", stack.size);
+    printf("Stack top: %d \n", stack.top);
+    printf("Stack data pointer: %p \n", stack.data);
 }
 
 int main()
 {
     ArrayStack stack;
 
-    initializeStack(&stack, 10);
+    initializeStack(&stack, 5);
+
+    push(&stack, 10);
+    push(&stack, 12);
+
+    printStack(stack);
+
+    int popValue = pop(&stack);
+    printf("Value is pop from the stack %d \n", popValue);
 
     printStack(stack);
 
